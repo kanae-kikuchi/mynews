604d9080c9d8edd5660d7b256578f31ba955b783
kanae-kikuchi <kanae.19850814@gmail.com>
2022-05-16 14:52:16 +0900

    kadai09

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+public function add()
+{
+    return view('admin.profile.create');
+}
+
+public function create()
+{
+    return redilect('admin/profile/create');
+}
+
+public function edit()
+{
+    return view('admin.profile.edit');
+}
+
+public function update()
+{
+    return redilect('admin/profile/edit');
+
+Route::get('admin/profile/create','ProfileController@'add');
+Route::get('admin/profile/edit','ProfileController@edit');
+}
+}
