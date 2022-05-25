354d9f34d28dec21d1262d0bd2fb11401f47252c
kanae-kikuchi <kanae.19850814@gmail.com>
2022-05-25 18:05:07 +0900

    kadai10

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -5,9 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyProfile</title>
+        <title>MyNews</title>
     </head>
     <body>
-        <h1>MyProfile作成</h1>
+        <h1>MyProfile 編集</h1>
     </body>
 </html>
