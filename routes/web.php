c988ed7aa89275fe6b2d4ba2a32e3ebe88420340
kanae-kikuchi <kanae.19850814@gmail.com>
2022-06-15 11:24:20 +0900

    kadai13

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,9 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin','middleware' => 'auth'],function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::post('news/create', 'Admin\NewsController@create');
-    Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::post('profile/create', 'Admin\ProfileController@create');
 });
 
 
